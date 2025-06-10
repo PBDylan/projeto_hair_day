7fbf272d8a52f0b255f9183d936f277dc3614538
Dylan <nalyd27@outlook.com>
2025-06-10 18:35:25 -0300

    Configurando input de agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,4 +1,14 @@
+import dayjs from "dayjs";
+
 const form = document.querySelector("form");
+const selectedDate = document.getElementById("date");
+
+// Date atual para formatar o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
+
+// Define a data atual e minima como sendo a data atual.
+selectedDate.value = inputToday;
+selectedDate.min = inputToday;
 
 form.onsubmit = (event) => {
   // Previne o comportamento padrão do formulário de carregar a página.
